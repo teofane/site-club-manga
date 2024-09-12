6193b77676126b40ddd3f0ffa2f841af01a2a368
teofane <garriguesteofane@gmail.com>
2024-09-12 17:46:07 +0200

    Initial commit

---
+++ ClubManga/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-white">
+    <p class="text-2xl text-black">Hello<br>World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
