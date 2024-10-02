48e4a026842f545bf90a54d8da669494b98ed803
mohamany13 <mohamany13.13@gmail.com>
2024-10-02 19:26:32 +0200

    Update and rename peinture.vue to index.vue

---
+++ ClubManga/pages/index.vue
@@ -0,0 +1,28 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <section id="home" class="items-center justify-center flex min-h-screen bg-[#FFEBF1] px-4 sm:px-0">
+      <div class="text-center">
+        <h1 class="text-4xl sm:text-7xl text-[#13000A] text-center border-solid border-4 border-[#FB6F92] rounded-lg px-2 py-5">Club Manga <br> Lycée Dhuoda</h1>
+      </div>
+    </section>
+    
+    <section id="presentation" class="min-h-screen bg-[#FFC2D1] text-[#13000A] flex flex-col items-center justify-center">
+      <div class="text-center mb-4">
+        <h2 class="text-3xl mb-4">Présentation</h2>
+        <p class="max-w-2xl mx-auto p-4 text-gray-800 leading-relaxed text-lg md:text-xl">
+          Le Club Manga du Lycée Dhuoda est un espace pour les passionnés de mangas, d'anime, de dessin et de culture japonaise...
+        </p>
+      </div>
+    </section>
+
+    <section id="projects" class="min-h-screen bg-[#FFEBF1] text-[#13000A] flex flex-col items-center justify-center">
+      <h2 class="text-3xl mb-4">Projets</h2>
+      <p class="max-w-2xl mx-auto p-4 text-gray-800 leading-relaxed text-lg md:text-xl">Le club manga offre de nombreux projets :</p>
+      <!-- Ajouter des liens vers les projets ici -->
+    </section>
+  </div>
+</template>
+
+<script setup>
+</script>
