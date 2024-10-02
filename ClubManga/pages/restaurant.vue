8c14e62ecde82711f21b4f3fe0fa31699b58e2b9
mohamany13 <mohamany13.13@gmail.com>
2024-10-02 16:55:02 +0200

    Update and rename index.vue to restaurant.vue

---
+++ ClubManga/pages/restaurant.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-[#FFC2D1] flex flex-col items-center justify-center">
+    <h1 class="text-4xl text-[#13000A] mb-4">Projet Restaurant</h1>
+    <p class="text-lg text-gray-800">Description du projet Restaurarnt...</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
