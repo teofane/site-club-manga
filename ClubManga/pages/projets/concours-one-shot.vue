74b862a6a6b06caceb7125c64bd47d0dfa8666a2
mohamany13 <mohamany13.13@gmail.com>
2024-10-03 08:58:13 +0200

    Update concours-one-shot.vue

--- ClubManga/pages/projets/concours-one-shot.vue
+++ ClubManga/pages/projets/concours-one-shot.vue
@@ -1,6 +1,10 @@
-<!-- pages/projets/concours-one-shot.vue -->
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-white">
-    <h1 class="text-4xl">Concours de One Shots</h1>
+  <div>
+    <h1>Concours One Shot</h1>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'home',
+};
