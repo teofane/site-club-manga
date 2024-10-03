5fd59a9b788e4754745810f406f0a3f5799a2aa4
mohamany13 <mohamany13.13@gmail.com>
2024-10-03 08:57:56 +0200

    Update restaurant.vue

--- ClubManga/pages/projets/restaurant.vue
+++ ClubManga/pages/projets/restaurant.vue
@@ -1,6 +1,10 @@
-<!-- pages/projets/restaurant.vue -->
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-white">
-    <h1 class="text-4xl">Restaurant</h1>
+  <div>
+    <h1>Restaurant</h1>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'home',
+};
