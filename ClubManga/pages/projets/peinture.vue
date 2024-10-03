9bd2c5385b46fd5be185410d8a44fdd3722b91ed
mohamany13 <mohamany13.13@gmail.com>
2024-10-03 08:22:55 +0200

    Update and rename ClubManga/pages/projets/peinture/index.vue to ClubManga/pages/projets/peinture.vue

---
+++ ClubManga/pages/projets/peinture.vue
@@ -0,0 +1,6 @@
+<!-- pages/projets/peinture.vue -->
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-white">
+    <h1 class="text-4xl">Peinture</h1>
+  </div>
+</template>
