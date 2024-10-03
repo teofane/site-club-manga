52dd5be2a3e0c9a676352d379df64b578779b68d
mohamany13 <mohamany13.13@gmail.com>
2024-10-03 08:23:47 +0200

    Update and rename ClubManga/pages/projets/concours-one-shot/index.vue to ClubManga/pages/projets/concours-one-shot.vue

---
+++ ClubManga/pages/projets/concours-one-shot.vue
@@ -0,0 +1,6 @@
+<!-- pages/projets/concours-one-shot.vue -->
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-white">
+    <h1 class="text-4xl">Concours de One Shots</h1>
+  </div>
+</template>
