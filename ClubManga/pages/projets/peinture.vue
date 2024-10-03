3d4ede8d5b9379be60a28e9ba578edc7d1241c9e
mohamany13 <mohamany13.13@gmail.com>
2024-10-03 08:57:30 +0200

    Update peinture.vue

--- ClubManga/pages/projets/peinture.vue
+++ ClubManga/pages/projets/peinture.vue
@@ -1,6 +1,11 @@
-<!-- pages/projets/peinture.vue -->
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-white">
-    <h1 class="text-4xl">Peinture</h1>
+  <div>
+    <h1>Peinture</h1>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'home',
+};
+</script>
