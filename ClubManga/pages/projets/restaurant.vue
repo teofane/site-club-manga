a07e604db4e1466bacc18203d619fe6cef3efb00
mohamany13 <mohamany13.13@gmail.com>
2024-10-03 08:23:23 +0200

    Update and rename ClubManga/pages/projets/restaurant/index.vue to ClubManga/pages/projets/restaurant.vue

---
+++ ClubManga/pages/projets/restaurant.vue
@@ -0,0 +1,6 @@
+<!-- pages/projets/restaurant.vue -->
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-white">
+    <h1 class="text-4xl">Restaurant</h1>
+  </div>
+</template>
